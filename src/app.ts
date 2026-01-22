eb310014bd28c935649fa89237ff3af335a7d14a
Masum Billah <masumbillah2062003@gmail.com>
2026-01-22 23:34:31 +0600

    problem solved

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://mupi-client.vercel.app"],
     credentials: true,
   }),
 );
